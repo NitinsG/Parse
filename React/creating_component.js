f7044407db35f106642e581db706ca8982f06a9b
Nitin Sharma <nitin_sharma-ggn@external.mckinsey.com>
2018-03-03 16:04:47 +0530

    Added more code

--- React/creating_component.js
+++ React/creating_component.js
@@ -64,3 +64,57 @@ application.react.js
    });
 
 module.exports = Application;
+
+/*
+
+Notice that the Collection component cannot directly mutate the Application component's state. 
+The Collection component has read-only access to that state via this.props object, 
+and the only way to update the parent component's state is to call the callback functions that are passed by the parent component. 
+In the Collection component, these callback functions are this.props.onRemoveTweetFromCollection and this.props. onRemoveAllTweetsFromCollection.
+
+*/
+
+// Let's start by creating the Stream.react.js  
+
+   var React = require('react');
+   var SnapkiteStreamClient = require('snapkite-stream-client');
+   var StreamTweet = require('./StreamTweet.react');
+   var Header = require('./Header.react');
+   
+   var Stream = React.createClass({
+     
+     getInitialState: function () {
+       return {
+          tweet: null 
+       }
+     },
+     
+     componentDidMount: function () {
+       SnapkiteStreamClient.initializeStream(this.handleNewTweet);
+     },
+     
+     componentWillUnmount: function () {
+       SnapkiteStreamClient.destroyStream();
+     },
+
+     handleNewTweet: function (tweet) {
+       this.setState({
+           tweet: tweet
+       });
+     },
+
+     render: function () {      
+        var tweet = this.state.tweet;
+        if (tweet) {
+           return (<StreamTweet tweet={tweet} onAddTweetToCollection={this.props.onAddTweetToCollection} />); 
+        }
+        return (<Header text="Waiting for public photos from Twitter..." />); 
+      }
+   });
+ 
+   module.exports = Stream;
+
+
+
+
+
